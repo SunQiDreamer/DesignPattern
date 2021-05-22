542ae03191783fa6c13661439274184093529729
孙琦 <993473797@qq.com>
2021-05-22 14:17:59 +0800

    Update

---
+++ 设计模式/行为型/模板模式/模板模式/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  模板模式
+//
+//  Created by 孙琦 on 2021/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
