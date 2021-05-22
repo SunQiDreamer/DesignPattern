1547c7b24c67bd0a2343b8c6e9967078c0db8122
孙琦 <993473797@qq.com>
2021-05-22 15:15:23 +0800

    Update 命令模式

--- 设计模式/行为型/命令模式/命令模式/main.m
+++ 设计模式/行为型/命令模式/命令模式/main.m
@@ -7,10 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#import "Receiver.h"
+#import "ConcreteCommnad.h"
+#import "Invoker.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        //具体执行者（员工）
+        Receiver *r = [[Receiver alloc] init];
+        //指令（老板要做的事情）
+        Commnad *c1 = [[ConcreteCommnad alloc] initWithReceiver:r name:@"c1"];
+        Commnad *c2 = [[ConcreteCommnad alloc] initWithReceiver:r name:@"c2"];
+        Commnad *c3 = [[ConcreteCommnad alloc] initWithReceiver:r name:@"c3"];
+        //发起者 （boss）
+        Invoker *i = [[Invoker alloc] init];
+        i.command = c1; // boss说我们要做点啥
+        i.command = c2;
+        i.command = c3;
+        [i executeCommand]; // 老板驱动任务
+        
+        [i cancelCommand:c2];
+        [i executeCommand];
+        
+        return 0;
     }
 }
